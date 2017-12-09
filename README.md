6cea54fc5a62240018969e3ec4647940f1c69fac
howoh <hwdep001@gmail.com>
2017-12-09 16:44:14 +0900

    add firebase

---
+++ README.md
@@ -0,0 +1,5 @@
+## [Firebase && Angularfire2](https://github.com/angular/angularfire2/blob/master/docs/ionic/v3.md)
+
+```bash
+$ npm install angularfire2 firebase promise-polyfill --save
+```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { MyApp } from './app.component';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MyApp } from './app.component';
+
+// firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+// environments
+import { environment } from './../environments/environment';
 
 // pages
 import { SigninPage } from './../pages/signin/signin';
@@ -18,7 +26,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp, {tabsPlacement: 'top'}),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
